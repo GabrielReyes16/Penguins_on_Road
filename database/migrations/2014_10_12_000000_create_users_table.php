12c4996657d2c6d89e06cdeea72e5ed3289a8677
Marcelo_Sanabria <108100801+MarceloxASG@users.noreply.github.com>
2023-06-18 20:17:37 -0500

    name xd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
+            $table->string('name');
             $table->string('apellidos');
             $table->string('especialidad');
             $table->string('email')->unique();
