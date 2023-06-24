475293d84b1d9e44c874fd1c075bbe8b7b40de58
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 01:58:25 -0500

    migra de user default

--- database/migrations/2023_06_20_000000_create_users_table.php
+++ database/migrations/2023_06_20_000000_create_users_table.php
@@ -12,19 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('id_usuario');
-            $table->string('nombres', 50);
-            $table->string('apellidos', 50);
-            $table->string('biografia', 350);
-            $table->string('especialidad', 50);
-            $table->string('rol');
+            $table->id();
+            $table->string('nombres');
+            $table->string('apellidos');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('licencia_conducir')->nullable();
-            $table->string('id_empresa')->nullable();
-            $table->binary('foto_perfil')->default('');
-            $table->binary('foto_banner')->default('');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -35,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('usuarios');
+        Schema::dropIfExists('users');
     }
 };
