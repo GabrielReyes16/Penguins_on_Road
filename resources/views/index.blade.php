f66b221a40866cd254b206c3693b24ff39e2375a
Azul-20 <108013005+Azul-20@users.noreply.github.com>
2023-06-24 00:39:27 -0500

    actualizacion index 4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styles-login.css">
+    <link rel="stylesheet" href="{{ asset('css/styles-login.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,300&display=swap" rel="stylesheet">
@@ -13,11 +13,12 @@
 <body>
     <div class="fondo-imagen">
         <header>
-            <a href="index.html"><img class="logo-proyecto" src="../../public/logo-proyecto-blanco.png" alt=""></a>
+            <a href="index.html"><img class="logo-proyecto" src="../../public/img/logo-proyecto-blanco.png" alt=""></a>
             <div>
                 <h1>Hola, Bienvenido!</h1>
             </div>
         </header>
+
         <div class="cuerpo">
             <div class="loginBox">
                 <div class="fondo-titulo">
@@ -49,8 +50,9 @@
             </div>
         </div>
     </div>
+
     <footer>
-        <img class="logo-tecsup" src="../../public/logotipo-TECSUP-blanco.png">
+        <img class="logo-tecsup" src="../../public/img/logotipo-TECSUP-blanco.png">
     </footer>
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
