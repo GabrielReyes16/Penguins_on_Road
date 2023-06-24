ad124c3be76dc1f9eb394d889b7e069c02cceb64
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 01:34:58 -0500

    cambio en usuario, para volverlo a su default

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\Usuario as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Usuario extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
