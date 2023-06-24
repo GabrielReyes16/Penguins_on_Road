8a9a6619fee47866b106d16358a0064b9aede9bf
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 02:28:42 -0500

    user model, auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\Usuario as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
