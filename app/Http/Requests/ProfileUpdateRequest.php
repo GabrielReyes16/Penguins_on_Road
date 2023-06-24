db183c13fd0ebf37b95615d10cbe5456a963b2b9
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 04:04:37 -0500

    intento de poder editar el apellido desde el profile

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,7 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'nombres' => ['string', 'max:255'],
-            'apelidos' => ['string', 'max:255'],
+            'apellidos' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
