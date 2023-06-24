b2dd71291359a54cfe70399b140384089645e2d1
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 00:31:25 -0500

    Ruteo del welcome

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
