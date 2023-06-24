cbc57670cd39c9b23803690e11f51a0d59569411
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 02:45:42 -0500

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'nombre',
+        'nombres',
+        'apellidos',
         'email',
         'password',
     ];
