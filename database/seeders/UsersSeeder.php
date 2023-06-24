ff1897f155b0c55205c5bf6b6284747c9f8cdb95
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 04:57:31 -0500

    seeder de users, recien se va a probar...

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersSeeder extends Seeder
 {
@@ -12,6 +15,28 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for ($i = 0; $i < 50; $i++) {
+            $letraInicial = Str::randomElement(['P', 'C', 'A']);
+            $digitos = Str::random(5, '0123456789');
+            $ID = $letraInicial . $digitos;
+            // Verificar si el nuevo ID está en uso
+            $existingUser = User::where('ID', $ID)->first();
+
+            while ($existingUser) {
+            $letraInicial = Str::randomElement(['P', 'C', 'A']);
+            $digitos = Str::random(5, '0123456789');
+            $ID = $letraInicial . $digitos;
+            // Verificar si el nuevo ID está en uso
+            $existingUser = User::where('ID', $ID)->first();
+}
+
+            User::create([
+                'id' => $ID,
+                'nombres' => "usuario$i",
+                'apellidos' => "apellido$i",
+                'email' => "email$i@test.com",
+                'password' => Hash::make("pass$i")
+            ]);
+        }
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Usuarios') }}
+                    </x-nav-link>
                 </div>
             </div>
 
