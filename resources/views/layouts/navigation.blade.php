eadb24ca674628acbae232345b0a7f7a634d9247
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 04:26:58 -0500

    nada.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    
                 </div>
             </div>
 
