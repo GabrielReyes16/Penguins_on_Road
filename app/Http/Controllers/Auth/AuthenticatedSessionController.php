849ef4ca86a1123367f32a35b540693e1906d357
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 06:10:09 -0500

    cambios en auth session para que al cerrar sesion, se dirija a login desde la primera parte del destroy auth session

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -43,7 +43,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
     public function logout(Request $request)
     {
