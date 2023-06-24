f05848802272de3e57e33d568a310bc0d551fbe5
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-24 05:18:54 -0500

    mod de la migracion de users

--- database/migrations/2023_06_24_071441_create_table_users.php
+++ database/migrations/2023_06_24_071441_create_table_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('id', 6)->primary();
             $table->string('nombres');
             $table->string('apellidos');
             $table->string('email')->unique();
