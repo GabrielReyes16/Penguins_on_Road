b54d029c913ce60bd2e16479c74c48019f3c2513
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-26 02:58:40 -0500

    cambios en la navbar de dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,8 +15,8 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Usuarios') }}
+                    <x-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">
+                        {{ __('Users') }}
                     </x-nav-link>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/welcome', function () {
 
 Route::get('/users', function () {
     return view('Users/index');
-})->middleware(['auth', 'verified'])->name('Users/index');
+})->middleware(['auth', 'verified'])->name('Users.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -34,6 +34,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
