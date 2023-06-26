d2fabb5fb3426d027230d292905fe1694cbc7345
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-26 02:05:13 -0500

    Cambios en usercontroller, e intento de añadir una vista o CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,69 @@ class UserController extends Controller
     {
         $users = User::all();
 
-        return view('usuarios.index', compact('users'));
+        return view('User.index', compact('users'));
+    }
+    public function create()
+    {
+        return view('User.crear');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required', 'rol' => 'required', 'email' => 'required'
+        ]);
+
+        $user = $request->all();
+        User::create($user);
+        return redirect()->route('User.index');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return view('User.editar', compact('User'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required', 'rol' => 'required', 'email' => 'required'
+        ]);
+
+        $usuario = $request -> all();
+
+        $user->update($usuario);
+
+        return redirect()->route('User.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('User.index');
     }
 }
+
 
