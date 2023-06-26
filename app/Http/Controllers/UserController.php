b1334c92a0c05ac6df282516698c10bf49fd7d67
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-26 02:06:50 -0500

    paginacion de users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(20);
 
         return view('User.index', compact('users'));
     }
