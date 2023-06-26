550a91c1e433daa15ec8acfff2caa9adbecc4c6c
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-26 02:12:35 -0500

    cambios de ruteo paar ver crud de users...

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,14 @@ Route::get('/welcome', function () {
 });
 
 Route::get('/dashboard', function () {
+    return view('Users/index');
+})->middleware(['auth', 'verified'])->name('Users/index');
+
+Route::get('/users', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::resource('/users', [UserController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
