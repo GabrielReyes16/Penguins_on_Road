f0ba333e832046f3d1a3eb8d10cfccc02a63c823
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-26 02:23:14 -0500

    modificaciones en web

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('/usuarios', UserController::class);
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
