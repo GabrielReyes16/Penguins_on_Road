88d54ddcf57b593add40a750f361adcfd202df49
Gabriel Reyes <114525321+GabrielReyes16@users.noreply.github.com>
2023-06-26 02:09:39 -0500

    cambio en web, ruteo para ver users

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::resource('/users', UserController::class);
+    Route::resource('/users', [UserController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
