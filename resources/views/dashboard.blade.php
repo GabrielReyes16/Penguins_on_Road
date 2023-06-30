0e15c9e8876cbe205e6f2bef4dc75a77ee200fbf
GabrielReyes16 <gabriel.reyes.b@tecsup.edu.pe>
2023-06-30 09:37:41 -0500

    cambio en dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-200">
-                    {{ __("You're logged in! Felicidadees bro :D") }}
+                    {{ __("Bienvenido. Penguins on Road es el software de gestión de buses de Tecsup. Podras registrar tu abordaje, ver tu boletas de viaje y más cosas!. Espero disfrutes del software") }}
                 </div>
             </div>
         </div>
