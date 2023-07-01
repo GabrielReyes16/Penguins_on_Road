56ea3ea0c22c2290dd51038cc308d92d5f0ca06c
GabrielReyes16 <gabriel.reyes.b@tecsup.edu.pe>
2023-07-01 01:10:54 -0500

    Intento de adaptar la navbar, inicio de CRUD de choferes

--- app/Http/Controllers/ChoferController.php
+++ app/Http/Controllers/ChoferController.php
@@ -7,45 +7,45 @@ use Illuminate\Http\Request;
 
 /**
  * Class ChofereController
- //* @package App\Http\Controllers
+ * @package App\Http\Controllers
  */
 class ChoferController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-    //  * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $choferes = Chofer::paginate();
 
-        return view('chofere.index', compact('choferes'))
+        return view('chofer.index', compact('choferes'))
             ->with('i', (request()->input('page', 1) - 1) * $choferes->perPage());
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     //* @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
         $chofere = new Chofer();
-        return view('chofer.create', compact('chofere'));
+        return view('chofere.create', compact('chofere'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     //* @param  \Illuminate\Http\Request $request
-     //* @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        request()->validate(Chofer::$rules);
+        request()->validate(Chofere::$rules);
 
-        $chofere = Chofer::create($request->all());
+        $chofere = Chofere::create($request->all());
 
         return redirect()->route('choferes.index')
             ->with('success', 'Chofere created successfully.');
@@ -54,12 +54,12 @@ class ChoferController extends Controller
     /**
      * Display the specified resource.
      *
-     //* @param  int $id
-     //* @return \Illuminate\Http\Response
+     * @param  int $id
+     * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        $chofere = Chofer::find($id);
+        $chofere = Chofere::find($id);
 
         return view('chofere.show', compact('chofere'));
     }
@@ -67,12 +67,12 @@ class ChoferController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     //* @param  int $id
-    // * @return \Illuminate\Http\Response
+     * @param  int $id
+     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        $chofere = Chofer::find($id);
+        $chofere = Chofere::find($id);
 
         return view('chofere.edit', compact('chofere'));
     }
@@ -80,28 +80,28 @@ class ChoferController extends Controller
     /**
      * Update the specified resource in storage.
      *
-    // * @param  \Illuminate\Http\Request $request
-    // * @param  Chofere $chofere
-     //* @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @param  Chofere $chofere
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Chofer $chofer)
+    public function update(Request $request, Chofere $chofere)
     {
-        request()->validate(Chofer::$rules);
+        request()->validate(Chofere::$rules);
 
-        $chofer->update($request->all());
+        $chofere->update($request->all());
 
         return redirect()->route('choferes.index')
             ->with('success', 'Chofere updated successfully');
     }
 
     /**
-     //* @param int $id
-     //* @return \Illuminate\Http\RedirectResponse
-     //* @throws \Exception
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
      */
     public function destroy($id)
     {
-        $chofere = Chofer::find($id)->delete();
+        $chofere = Chofere::find($id)->delete();
 
         return redirect()->route('choferes.index')
             ->with('success', 'Chofere deleted successfully');
