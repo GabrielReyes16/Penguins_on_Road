7adb1c20f839292f34077986c9721dd50e4948eb
Azul-20 <rael.rivero01@gmail.com>
2023-07-02 04:44:12 -0500

    c

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,13 +34,13 @@
                         <!-- Email Address -->
                         <div class="inputLine1">
                             <x-input-label for="email" :value="__('Usuario')" class="textInput" />
-                            <x-text-input id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                            <x-text-input class="input" id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
                             <x-input-error :messages="$errors->get('email')" />
                         </div>
                                 <!-- Password -->
                         <div class="inputLine2">
                             <x-input-label for="password" :value="__('Contraseña')" class="textInput" />
-                            <x-text-input id="password"
+                            <x-text-input class="input" id="password"
                                 type="password"
                                 name="password"
                                 required autocomplete="current-password" />
