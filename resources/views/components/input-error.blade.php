7146bbaee2c7d70872793ad2ad3debd34180533a
Azul-20 <rael.rivero01@gmail.com>
2023-07-06 18:10:08 -0500

    c

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -3,6 +3,6 @@
 
 @if ($messages)
     <label style="color: red; justify-content: center;align-items: center">
-        {{ __('Lo sentimos pero su usuario o contraseña no son correctas. Vuelva a intentarlo') }}
+        {{ __('Lo sentimos pero su usuario o contraseña no son correctas. Vuelva a intentarlo.') }}
     </label>
 @endif
