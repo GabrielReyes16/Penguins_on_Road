3946daf2bce2eb9391d4c64c5b3977fdb378aa10
harold18m <harold.medrano@tecsup.edu.pe>
2023-07-09 18:39:08 -0500

    Vamos a terminarlo aqui creo

--- app/Http/Controllers/ViajeController.php
+++ app/Http/Controllers/ViajeController.php
@@ -2,85 +2,86 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Viaje;
+use App\Models\Chofer;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use App\Models\Viaje;
+use Illuminate\Support\Facades\Auth;
 
-/**
- * Class ViajeController
- * @package App\Http\Controllers
- */
 class ViajeController extends Controller
 {
-    public function index()
+    public function mostrarViajes()
     {
-        $viajes = Viaje::paginate();
+        $idUsuario = Auth::id();
 
-        return view('viaje.index', compact('viajes'))
-            ->with('i', (request()->input('page', 1) - 1) * $viajes->perPage());
+        $chofer = Chofer::where('id_usuario', $idUsuario)->first();
+
+        if (!$chofer) {
+            return redirect()->back()->with('error', 'No se encontró el chofer.');
+        }
+
+        $viajes = Viaje::where('id_chofer', $chofer->id_chofer)
+            ->orderByDesc('id_viaje')
+            ->get();
+
+        $viajes->transform(function ($viaje) {
+            $duracion = Carbon::parse($viaje->hora_final)->diff(Carbon::parse($viaje->hora_inicio));
+            $viaje->duracion = $duracion->format('%H:%I:%S');
+            $viaje->fecha_viaje = Carbon::createFromFormat('Y-m-d', $viaje->fecha_viaje)->format('d/m/Y');
+            return $viaje;
+        });
+
+        return view('usuario-chofer.mostrar-viajes', compact('viajes'));
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     */
-    public function create()
+
+    public function crearViaje(Request $request)
     {
+        $idUsuario = $request->input('id_usuario');
+        
+        $chofer = Chofer::where('id_usuario', $idUsuario)->first();
+
+        $bus = $chofer->bus;
+
+        $ruta = $bus->ruta;
+
         $viaje = new Viaje();
-        return view('viaje.create', compact('viaje'));
+        $viaje->id_chofer = $chofer->id_chofer;
+        $viaje->id_bus = $bus->id_bus;
+        $viaje->id_ruta = $ruta->id_ruta;
+        $viaje->fecha_viaje = Carbon::now()->format('Y-m-d');
+        $viaje->save();
+
+        return redirect()->back()->with('success', 'El viaje se ha creado correctamente.');
     }
-    /**
-     * Store a newly created resource in storage.
-     *
-     */
-    public function store(Request $request)
-    {
-        $viaje = Viaje::create($request->all());
+    public function comenzarViaje(Request $request, $idViaje)
+{
+    $viaje = Viaje::findOrFail($idViaje);
+    $viaje->hora_inicio = Carbon::now();
+    $viaje->save();
 
-        return redirect()->route('viajes.index')
-            ->with('success', 'Viaje created successfully.');
-    }
+    return redirect()->back()->with('success', 'El viaje ha comenzado exitosamente.');
+}
+public function terminarViaje(Request $request, $idViaje)
+{
+    $viaje = Viaje::findOrFail($idViaje);
+    $viaje->hora_final = Carbon::now();
+    $viaje->save();
 
-    /**
-     * Display the specified resource.
-     *
-     */
-    public function show($id)
-    {
-        $viaje = Viaje::find($id);
+    return redirect()->back()->with('success', 'El viaje ha sido terminado exitosamente.');
+}
+public function actualizarEstadoViaje(Request $request)
+{
+    $viajeId = $request->input('id_viaje');
+    $estado = $request->input('estado_viaje');
 
-        return view('viaje.show', compact('viaje'));
-    }
+    // Obtén el viaje y actualiza el estado
+    $viaje = Viaje::findOrFail($viajeId);
+    $viaje->estado = $estado;
+    $viaje->save();
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        $viaje = Viaje::find($id);
-        return view('viaje.edit', compact('viaje'));
-    }
+    // Redirecciona a la página deseada o muestra un mensaje de éxito
+    return redirect()->back()->with('success', 'El estado del viaje se ha actualizado correctamente.');
+}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     */
-    public function update(Request $request, Viaje $viaje)
-    {
-        $viaje->update($request->all());
 
-        return redirect()->route('viajes.index')
-            ->with('success', 'Viaje updated successfully');
-    }
-
-    /**
-     * @param int $id
-     * @return \Illuminate\Http\RedirectResponse
-     * @throws \Exception
-     */
-    public function destroy($id)
-    {
-        $viaje = Viaje::find($id)->delete();
-
-        return redirect()->route('viajes.index')
-            ->with('success', 'Viaje deleted successfully');
-    }
 }
