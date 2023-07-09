7caee87e9af98c06d32ae44640d1c21e4c32bd91
Azul-20 <rael.rivero01@gmail.com>
2023-07-09 18:01:45 -0500

    c

--- resources/views/usuario-pasajero/navbar-home.blade.php
+++ resources/views/usuario-pasajero/navbar-home.blade.php
@@ -139,7 +139,7 @@
         </div>
     </div>
 
-
+    <script src="{{ asset('js/animaciones.js') }}"></script>
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
