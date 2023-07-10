76a27082e9091c4daba4af18da373297485b909c
GabrielReyes16 <gabriel.reyes.b@tecsup.edu.pe>
2023-07-10 00:23:50 -0500

    ruteo de viajes admin

--- app/Http/Controllers/Admin/ViajeAdminController.php
+++ app/Http/Controllers/Admin/ViajeAdminController.php
@@ -7,6 +7,7 @@ use App\Models\Chofer;
 use App\Models\Viaje;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 
 class ViajeAdminController extends Controller
 {
@@ -24,4 +25,53 @@ class ViajeAdminController extends Controller
 
         return view('admin.viaje.mostrar-viajes', compact('viajes'));
     }
+    public function crearViaje(Request $request)
+    {
+        $idUsuario = $request->input('id_usuario');
+        
+        $chofer = Chofer::where('id_usuario', $idUsuario)->first();
+
+        $bus = $chofer->bus;
+
+        $ruta = $bus->ruta;
+
+        $viaje = new Viaje();
+        $viaje->id_chofer = $chofer->id_chofer;
+        $viaje->id_bus = $bus->id_bus;
+        $viaje->id_ruta = $ruta->id_ruta;
+        $viaje->fecha_viaje = Carbon::now()->format('Y-m-d');
+        $viaje->save();
+
+        return redirect()->back()->with('success', 'El viaje se ha creado correctamente.');
+    }
+    public function comenzarViaje(Request $request, $idViaje)
+{
+    $viaje = Viaje::findOrFail($idViaje);
+    $viaje->hora_inicio = Carbon::now();
+    $viaje->save();
+
+    return redirect()->back()->with('success', 'El viaje ha comenzado exitosamente.');
 }
+public function terminarViaje(Request $request, $idViaje)
+{
+    $viaje = Viaje::findOrFail($idViaje);
+    $viaje->hora_final = Carbon::now();
+    $viaje->save();
+
+    return redirect()->back()->with('success', 'El viaje ha sido terminado exitosamente.');
+}
+public function actualizarEstadoViaje(Request $request)
+{
+    $viajeId = $request->input('id_viaje');
+    $estado = $request->input('estado_viaje');
+
+    $viaje = Viaje::findOrFail($viajeId);
+    $viaje->estado = $estado;
+    $viaje->save();
+
+    return redirect()->back()->with('success', 'El estado del viaje se ha actualizado correctamente.');
+}
+
+
+}
+
