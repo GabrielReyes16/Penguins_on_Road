84787c62d4b063129142d99be19c8d50b21e1d0c
GabrielReyes16 <gabriel.reyes.b@tecsup.edu.pe>
2023-07-09 23:47:09 -0500

    viajes - admin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -56,7 +56,7 @@
                     </div>
 
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-nav-link :href="route('admin.viajes.mostrarViajes')" :active="request()->routeIs('admin.viajes.mostrarViajes')">
+                        <x-nav-link :href="route('admin.viajes.index')" :active="request()->routeIs('admin.viajes.index')">
                             {{ __('Viajes') }}
                         </x-nav-link>
                     </div>
@@ -142,8 +142,8 @@
                 </x-responsive-nav-link>
             </div>
             <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link :href="route('admin.boletasviaje.index')" :active="request()->routeIs('admin.boletasviaje.index')">
-                    {{ __('Boletas de Viaje') }}
+                <x-responsive-nav-link :href="route('admin.viajes.index')" :active="request()->routeIs('admin.viajes.index')">
+                    {{ __('Viajes') }}
                 </x-responsive-nav-link>
             </div>
             <div class="pt-2 pb-3 space-y-1">
